9b603ce7993d7a65112e6e9ce49658f511224037
Andrew Strong <andrewstrongdev@gmail.com>
2022-06-29 14:33:09 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("hello world!");
+
+//query selector- grab the html element
+
+const inputField = document.querySelector("input");
+
+//Step 2: Write out your function
+function addMovie(event) {
+
+
+}
+
+//Step 3: Combine step one and step two using addEventListener
